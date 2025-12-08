412d6754d1c04781cadccd61e06e2725848f2272
Mary Jane <ruplet@ruplet.com>
2025-12-08 23:17:38 +0100

    Last fixes to bounded arith

--- listings/lean-example.lean
+++ listings/lean-example.lean
@@ -3,17 +3,21 @@
 theorem pred_exists :
   ∀ {x : M}, x ≠ 0 → ∃ y ≤ x, x = y + 1 :=
 by
+  -- peano.Formula (Vars2 .y .x) is the type of deeply-embedded formulas
+  -- with two free variables, "y" and "x"
   let ind1 : peano.Formula (Vars2 .y .x) := x =' (y + 1)
+  -- similarly below, "y" cannot be free in `ind2`.
+  -- `iBdEx' t phi` denotes `∃ var ≤ t . phi` for some variable name `var`
   let ind2 : peano.Formula (Vars1 .x) :=
     (Formula.iBdEx' x (display2 .y ind1).flip)
+  -- `display1` takes a formula with 1 free variable and makes it explicit
+  -- that it is intended to be bound by the induction axiom scheme
   let ind := idelta0.delta0_induction $ display1 $ (x ≠' 0) ⟹ ind2
 
+  -- prove that the formula actually is Delta0
   unfold ind2 ind1 at ind
-
   specialize ind (by
     rw [IsDelta0.display1]
-    -- TODO: this lemma can't be in @[delta0_simps],
-    -- as it creates a goal 'φ.IsOpen' - which might be not true!
     rw [IsDelta0.of_open.imp]
     · constructor
       · unfold Term.neq
@@ -25,8 +29,9 @@ by
       rw [IsOpen.not]
       constructor; constructor
   )
+  -- use our custom macro to simplify `ind` to use it in proving
   simp_induction at ind
-
+  -- proceed with the proof by induction; name subgoals; clear helpers
   apply ind ?base ?step <;> clear ind ind1 ind2
   · simp only [IsEmpty.forall_iff]
   · intro a hind h
